b06dd5504f221ee3ef37ae455adaddd4e0ff4e2b
Justin Fear <justin.m.fear@gmail.com>
2016-12-30 16:09:06 -0500

    cleaning up.

--- sramongo/sra2mongo.py
+++ sramongo/sra2mongo.py
@@ -5,12 +5,13 @@ import argparse
 from argparse import RawDescriptionHelpFormatter as Raw
 from urllib.error import HTTPError
 from xml.etree import ElementTree
+from logging import INFO, DEBUG
 
 from Bio import Entrez
 from mongoengine import connect
 
 from sramongo.logger import logger
-from sramongo.mongo import start_mongo
+from sramongo.mongo import MongoDB
 from sramongo.sra import SraExperiment
 from sramongo.mongo_schema import Experiment, Sample, Organization, Run, Study
 
@@ -38,7 +39,9 @@ def arguments():
     parser.add_argument("--debug", dest="debug", action='store_true', required=False,
                         help="Turn on debug output.")
 
-    return parser.parse_args(['--db', '/data/db', '--dbLog', '/data/log', '--email', 'justin.fear@nih.gov', '--query', '"Drosophila melanogaster"[Orgn]', '--debug'])
+    return parser.parse_args(['--db', '/data/db', '--dbLog', '/data/log', '--email', 'justin.fear@nih.gov',
+                              '--query', '"Drosophila melanogaster"[Orgn]', '--debug'])
+
 
 def download_sra(records):
     records = sra_records
@@ -68,46 +71,74 @@ def download_sra(records):
         out_handle.write(data)
     out_handle.close()
 
+def query_sra(query):
+    handle = Entrez.esearch(db='sra', term=query, usehistory="y")
+    records = Entrez.read(handle)
+    handle.close()
+    return records
+
+def fetch_sra(records):
+    handle = Entrez.efetch(db='sra', retmax=10,
+                           webenv=records['WebEnv'],
+                           query_key=records['QueryKey'])
+
+    records = handle.read()
+    handle.close()
+    return records
+
+def add_pkg_to_database(pkg):
+    sraExperiment = SraExperiment(pkg))
+
+    # Add Experiment Package to database
+    # Build study document
+    submission = Submission(**sraExperiment.submission)
+    organization = Organization(**sraExperiment.organization)
+    study = Study(submission=submission, organization=organization, **sraExperiment.study)
+    study.save()
+
+    # Build sample document
+    sample = Sample(**sraExperiment.sample)
+    sample.save()
+
+    # Build experiment document
+    experiment = Experiment(study=study, **sraExperiment.experiment)
+    experiment.samples = sraExperiment.pool
+    experiment.save()
+
+    # Add experiment id to study
+    study.experiments.append(experiment.id)
+
+    # Build run documents
+    for r in sraExperiment.run:
+        run = Run(experiment=experiment, **r)
+        run.save()
+        experiment.runs.append(run)
+    experiment.save()
+
 def main():
     # Import commandline arguments.
     args = arguments()
+
+    # Set logging level
+    if args.debug:
+        logger.setLevel(DEBUG)
+    else:
+        logger.setLevel(INFO)
+
     Entrez.email = args.email
-    query = args.query
 
-    # Start Mongo
-    db = args.db
-    dblog = args.dbLog
-    if not os.path.exists(db):
-        os.mkdir(db)
+    logger.info('Starting MongoDB')
+    with MongoDB(dbDir=args.dbDir, logDir=args.logDir, port=args.port):
+        client = connect('sra')
+        try:
+            sra_query = query_sra(args.query)
+            sra_records = fetch_sra(sra_query)
 
-    dblog = args.dbLog
-    if not dblog is None:
-        if not os.path.exists(dblog):
-            os.mkdir(dblog)
-
-    pid = start_mongo(dbDir=db, logDir=dblog)
-    client = connect('sra')
-
-    try:
-        # query SRA
-        search_handle = Entrez.esearch(db='sra', retmax=10, term=query, usehistory="y")
-        search_records = Entrez.read(search_handle)
-        search_handle.close()
-
-        webenv = search_records['WebEnv']
-        query_key = search_records['QueryKey']
-
-        fetch_handle = Entrez.efetch(db='sra', retmax=10, webenv=webenv, query_key=query_key)
-        fetch_records = fetch_handle.read()
-        fetch_handle.close()
-
-        tree = ElementTree.fromstring(fetch_records)
-        for pkg in tree:
-            sra = SraExperiment(pkg)
-            study = Study(**sra.study).save()
-            org = Organization(**sra.organization).save()
-    except:
-        pass
-    finally:
-       client.close()
-       pid.kill()
+            tree = ElementTree.fromstring(sra_records)
+            sras = []
+            # Iterate over experiment packages
+            for exp_pkg in tree:
+        except:
+            pass
+        finally:
+           client.close()
