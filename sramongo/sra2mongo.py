4dad6e72e7d71647a9e8997cda3d0d6525e960d7
Justin Fear <justin.m.fear@gmail.com>
2017-01-04 08:30:41 -0500

    Adds caching so I don't ping server too much.

--- sramongo/sra2mongo.py
+++ sramongo/sra2mongo.py
@@ -4,23 +4,28 @@ import os
 import argparse
 from argparse import RawDescriptionHelpFormatter as Raw
 from urllib.error import HTTPError
+from http.client import IncompleteRead
 from xml.etree import ElementTree
 from logging import INFO, DEBUG
 import pandas as pd
 from io import StringIO
+from tempfile import NamedTemporaryFile
+import time
 
 from Bio import Entrez
 from mongoengine import connect
 
 from sramongo.logger import logger
 from sramongo.mongo import MongoDB
-from sramongo.sra import SraExperiment
+from sramongo.sra import SraExperiment, XMLSchemaException
 from sramongo.mongo_schema import Submission, Organization,  Study, \
     Sample, Pool, Experiment, Run
 
-STUDIES_ADDED = []
-EXPERIMENTS_ADDED = []
-RUNS_ADDED = []
+STUDIES_ADDED = set()
+EXPERIMENTS_ADDED = set()
+RUNS_ADDED = set()
+
+_DEBUG = False
 
 def arguments():
     """Pulls in command line arguments."""
@@ -52,7 +57,29 @@ def arguments():
                         help="Turn on debug output.")
 
     return parser.parse_args(['--dbDir', '/data/db', '--logDir', '/data/log', '--email', 'justin.fear@nih.gov',
-                              '--query', '"Drosophila melanogaster"[Orgn]'])
+                              '--query', '"Drosophila melanogaster"[Orgn]', '--debug'])
+
+
+class Cache(object):
+    def __init__(self):
+        if not os.path.exists('.cache/sra2mongo'):
+            os.makedirs('.cache/sra2mongo')
+        self.cachedir = os.path.abspath('.cache/sra2mongo')
+        self.cached = set([x for x in os.listdir(path=self.cachedir) if x.endswith('.txt')])
+
+    def add_to_cache(self, name, _type, data):
+        fname = '{}_{}.txt'.format(name, _type)
+        with open(os.path.join(self.cachedir, fname), 'w') as fh:
+            fh.write(data)
+        self.cached.add(fname)
+
+    def get_cache(self, name, _type):
+        fname = '{}_{}.txt'.format(name, _type)
+        if fname in self.cached:
+            with open(os.path.join(self.cachedir, fname), 'r') as fh:
+                return fh.read()
+        else:
+            return None
 
 
 def fetch_sra(records, runinfo_retmode='text', **kwargs):
@@ -63,29 +90,51 @@ def fetch_sra(records, runinfo_retmode='text', **kwargs):
                'query_key': records['QueryKey'], 'retmax': batch_size}
     options.update(kwargs)
 
+    cache = Cache()
     for start in range(0, count, batch_size):
         end = min(count, start+batch_size)
-        logger.info("Downloading record %i to %i" % (start+1, end))
-        attempt = 0
-        while attempt < 3:
-            attempt += 1
-            try:
-                xml_handle = Entrez.efetch(retstart=start, **options)
 
-                runinfo_handle = Entrez.efetch(rettype="runinfo", retmode=runinfo_retmode,
-                                               retstart=start, **options)
-            except HTTPError as err:
-                if 500 <= err.code <= 599:
-                    logger.warn("Received error from server %s" % err)
-                    logger.warn("Attempt %i of 3" % attempt)
-                    time.sleep(15)
-                else:
-                    raise
-        xml_data = xml_handle.read()
-        runinfo_data = runinfo_handle.read()
+        cache_xml = cache.get_cache(start, 'xml')
+        cache_runinfo = cache.get_cache(start, 'runinfo')
 
-        xml_handle.close()
-        runinfo_handle.close()
+        if (cache_xml is not None) and (cache_runinfo is not None):
+            logger.warn('Using cached for {} to {} instead of downloading. '
+                        'To force download please remove: {}'.format(start+1, end, cache.cachedir))
+            xml_data = cache_xml
+            runinfo_data = cache_runinfo
+        else:
+            logger.info("Downloading record %i to %i" % (start+1, end))
+            attempt = 0
+            while attempt < 3:
+                attempt += 1
+                try:
+                    xml_handle = Entrez.efetch(retstart=start, **options)
+
+                    runinfo_handle = Entrez.efetch(rettype="runinfo", retmode=runinfo_retmode,
+                                                   retstart=start, **options)
+
+                    xml_data = xml_handle.read()
+                    xml_handle.close()
+                    cache.add_to_cache(start, 'xml', xml_data)
+
+                    runinfo_data = runinfo_handle.read()
+                    runinfo_handle.close()
+                    cache.add_to_cache(start, 'runinfo', runinfo_data)
+
+                except HTTPError as err:
+                    if 500 <= err.code <= 599:
+                        logger.warn("Received error from server %s" % err)
+                        logger.warn("Attempt %i of 3" % attempt)
+                        time.sleep(15)
+                    else:
+                        raise
+                except IncompleteRead:
+                    if attempt < 3:
+                        logger.warn('Incomplete read, trying again. %s' % start)
+                        logger.warn("Attempt %i of 3" % attempt)
+                        time.sleep(15)
+                    else:
+                        raise
 
         yield xml_data, runinfo_data
 
@@ -118,15 +167,17 @@ def add_pkg_to_database(pkg, runinfo):
                                                      study=study, samples=pool)
 
     # Add experiment id to study
-    study = study.modify(push__experiments=experiment.experiment_id)
+    if study is not None:
+        study.modify(push__experiments=experiment.experiment_id)
+        STUDIES_ADDED.add(study.study_id)
 
     # Build run documents
     runs = Run.build_from_SraExperiment(sraExperiment, runinfo)
-    experiment = experiment.modify(push_all__runs=[run.run_id for run in runs])
+    RUNS_ADDED.update(set([run.run_id for run in runs]))
 
-    STUDIES_ADDED.append(study.study_id)
-    EXPERIMENTS_ADDED.append(experiment.study_id)
-    RUNS_ADDED.extend([run.run_id for run in runs])
+    if experiment is not None:
+        experiment.modify(push_all__runs=[run.run_id for run in runs])
+        EXPERIMENTS_ADDED.add(experiment.study_id)
 
 
 def main():
@@ -136,6 +187,8 @@ def main():
     # Set logging level
     if args.debug:
         logger.setLevel(DEBUG)
+        global _DEBUG
+        _DEBUG = True
     else:
         logger.setLevel(INFO)
 
