05bd0736a4194ad1ba6e52cb69d323f0abf83d07
Justin Fear <justin.m.fear@gmail.com>
2017-01-27 13:54:46 -0500

    travis again

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 set -x
 
+sudo ln -s /lib/x86_64-linux-gnu/libssl.so.1.0.0 /usr/lib/libssl.so.10
+sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.10
+
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
