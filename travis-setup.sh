2cdf9bb74b6b1c80e6cf81c3318e4398ff6566ad
Justin Fear <justin.m.fear@gmail.com>
2017-01-27 15:24:45 -0500

    bump3

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 set -x
 
-sudo apt-get install mongodb
-
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
