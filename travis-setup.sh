f35dfdabf9ef499d01dfadf3f7dc18fb62e845b8
Justin Fear <justin.m.fear@gmail.com>
2017-01-27 09:48:10 -0500

    setting up travis

--- travis-setup.sh
+++ travis-setup.sh
@@ -11,6 +11,7 @@ conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
 conda config --add channels bioconda
+conda config --add channels jfear
 
 conda install -y --file requirements.txt
 
