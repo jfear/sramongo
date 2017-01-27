1100fc6733c8f24561921cfad0db40df72a8431b
Justin Fear <justin.m.fear@gmail.com>
2017-01-27 16:04:35 -0500

    travis

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 set -x
 
+# Download and set up conda
+curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1204-3.4.1.tgz
+tar -xf mongodb-linux-x86_64-ubuntu1204-3.4.1.tgz && mv mongodb-linux-x86_64-ubuntu1204-3.4.1.tgz  ~/mongo
+export PATH=~/mongo/bin:$PATH
+
+# Download and set up conda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
