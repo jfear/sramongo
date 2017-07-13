d129b16eb60f71ed07646f6924fd51ce215f62b9
Justin Fear <justin.m.fear@gmail.com>
2017-07-13 16:21:25 -0400

    add bumpversion

--- conda-build-and-upload.sh
+++ conda-build-and-upload.sh
@@ -2,8 +2,7 @@
 
 # Always build with conda to test any build issues, but only upload if we're on
 # master branch and it's not a pull request.
-export SRAMONGO_VERSION=$(./_version.py version)
-export SRAMONGO_BUILD=$(./_version.py build)
+export SRAMONGO_BUILD=$(git rev-list --count `git describe --tags --abbrev=0`..HEAD --count)
 conda build conda-recipe
 
 if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
