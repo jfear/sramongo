aa45d72f26b189db99457e29ec897914516f80fb
Justin M. Fear <justin.m.fear@gmail.com>
2019-02-26 13:28:49 -0500

    Removes mongodb testing because I am removing mongodb from project.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,28 +3,7 @@
 
 import pytest
 
-from sramongo.mongo import start_mongo, stop_mongo
 from sramongo.xml_helpers import xml_to_root
-
-
-@pytest.fixture(scope='session')
-def mongo_folders(tmpdir_factory):
-    """ Create TMPDIR for database and logs. """
-    data = tmpdir_factory.mktemp('db')
-    log = tmpdir_factory.mktemp('log')
-    return str(data), str(log)
-
-
-@pytest.fixture(scope='session')
-def mongoDB(mongo_folders):
-    """ Start the database server.
-
-    Server will be killed after the fixture is no longer neaded.
-    """
-    mongoDB = start_mongo(dbDir=mongo_folders[0], logDir=mongo_folders[1])
-    yield mongoDB
-    print('Shutting down mongoDB.')
-    stop_mongo(dbDir=mongo_folders[0])
 
 
 @pytest.fixture(scope='session')
